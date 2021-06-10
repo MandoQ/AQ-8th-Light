0609590323a09ee66459c37f4b0b2e8cc86388ec
MandoQ <armando.quintana@wustl.edu>
2021-06-10 18:47:24 -0500

    Updated Fetch function and now displays book info

--- googleBooks.js
+++ googleBooks.js
@@ -15,11 +15,40 @@ function fetchBooks(query){
         res.on('data', chunk => {
             body += chunk;
         });
-
+        
         res.on('end', () => {
-            console.log(JSON.parse(body));
+            displayBookInformation(body);
         });
     });
+}
+
+function displayBookInformation(data){
+    var result = '';
+    let jsonResponse = JSON.parse(data);
+    let bookList = jsonResponse.items;
+    var publisher = '';
+
+    for(var i = 0; i < bookList.length; ++i){
+        result += "Title: " + bookList[i].volumeInfo.title + " Author(s): ";
+        let authors = bookList[i].volumeInfo.authors;
+
+        if(authors.length == 1){
+            result += authors[0];
+        }else{
+            for(var l = 0; l < authors.length; ++l){
+                result += " " + authors[l];
+            }
+        }
+        
+        if(bookList[i].volumeInfo.publisher){
+            publisher = "Publisher: " + bookList[i].volumeInfo.publisher;
+        }else{
+            publisher = "Publisher: n/a";
+        }
+
+        console.log(result + " " + publisher);
+        result = '';
+    }
 }
 
 function getUserQuery(){
