5976841a77089b0797bb7871c6806c92287d7f83
MandoQ <armando.quintana@wustl.edu>
2021-06-10 15:15:51 -0500

    Added manual book search with api

--- googleBooks.js
+++ googleBooks.js
@@ -1,3 +1,18 @@
+function fetchBooks(){
+    const https = require('https');
+   
+    https.get('https://www.googleapis.com/books/v1/volumes?q=handmaid', (res) => {
+        var body = '';
+        res.on('data', chunk => {
+            body += chunk;
+        });
+
+        res.on('end', () => {
+            console.log(JSON.parse(body));
+        });
+    });
+}
+
 function getUserQuery(){
     console.log("Enter a book you wish to search");
     process.stdin.on('data', userInput => {
@@ -6,4 +21,5 @@ function getUserQuery(){
     });
 }
 
-getUserQuery();
+//getUserQuery();
+fetchBooks();
